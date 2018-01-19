ce0cb56b0fc67c1abdac6250959685b5fef17bb4
iulia33 <panc_iulia@yahoo.com>
2018-01-19 11:08:12 +0200

    Update pixel.js

--- pixel.js
+++ pixel.js
@@ -3,11 +3,13 @@ window.addEventListener("load", function(){
 const submitBtn = document.getElementById("submit");
 const table = document.getElementById("pixel_canvas");
 
+  ////when submit is clicked create a table
 submitBtn.addEventListener("click", function(e){
   e.preventDefault();
   makeGrid();
 });
 
+  //given the user's input, create a grid
 function makeGrid(){
   const heightInp = document.getElementById("input_height");
   const widthInp = document.getElementById("input_width");
@@ -25,17 +27,19 @@ function makeGrid(){
 }
 
 table.addEventListener("click", function(e){
+  //when a cell is clicked add a clear button
   const clearBtn = document.getElementById("clear");
   clearBtn.classList.remove("clear");
   const colorPicker = document.getElementById("colorPicker");
   const colorPickerVal = colorPicker.value;
   let celClicked = e.target;
+  //check if the cell clicked has a background color, if it has, remove it, else add it
   if (celClicked.style.backgroundColor != ""){
     celClicked.style.backgroundColor = "";
   }else{
     celClicked.style.backgroundColor = colorPickerVal;
   }
-
+  //remove background color for all the cells, when clear button is clicked
   clearBtn.addEventListener("click", clearTable);
 });
 
