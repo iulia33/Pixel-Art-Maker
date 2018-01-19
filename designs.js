8ed994eeabcba258b3a9a63061056bf384887655
iulia33 <panc_iulia@yahoo.com>
2018-01-19 11:05:05 +0200

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,11 +1,12 @@
 /*global $*/
 $(document).ready(function(){
-
+//when submit is clicked create a table
 $("#submit").on('click',function(e){
   e.preventDefault();
   makeGrid()
 });
 
+//given the user's input, create a grid
 function makeGrid(){
   $("table").empty();
   const heightLength = $("#input_height").val();
@@ -20,13 +21,17 @@ function makeGrid(){
 }
 
 $("table").on("click", "td", function(){
+  //when a cell is clicked a clear button appears
   $("#clear").removeClass("clear");
+  //chose color for the design
   const colorPicker = $("#colorPicker").val();
+  //check if the cell has a background color, and if it does, remove it, else add it
   if ((typeof $(this).attr("bgcolor")==="undefined")||($(this).attr("bgcolor") === "")){
     $(this).attr("bgcolor",colorPicker);
   }else{
     $(this).attr("bgcolor", "" );
   }
+  //when clear button is clicked, clear all the cells
   $("#clear").on("click", clearTable);
 });
 
